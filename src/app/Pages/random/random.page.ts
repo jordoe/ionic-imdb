6e544f9066a39cb77f258c0a7eee9f0505e27a1b
Jorge D <jdomingueze@minsait.com>
2020-02-04 16:22:45 +0100

    random film method evolved

--- src/app/Pages/random/random.page.ts
+++ src/app/Pages/random/random.page.ts
@@ -19,16 +19,13 @@ export class RandomPage implements OnInit {
       this.film = response;
       //console.log(response);
     })
-    this.filmsService.searchTest().subscribe((response: any) => {
-      console.log(response);
-    })
   }
 
   public pressRandom(): void {
-    this.filmsService.getRandomFilm().subscribe((response: any) => {
+    this.filmsService.getRandomFilm(null, null, 1960).subscribe((response: any) => {
       this.filmImage = 'https://image.tmdb.org/t/p/original'+response.poster_path;
       this.film = response;
-      console.log(response);
+      //console.log(response);
     })
   }
 }
--- src/app/Services/films.service.ts
+++ src/app/Services/films.service.ts
@@ -17,14 +17,34 @@ export class FilmsService {
     const defaultFilmUrl = 'https://api.themoviedb.org/3/movie/'+ defaultFilm;
     return this.https.get(defaultFilmUrl + this.key);
   }
-  public getRandomFilm(): Observable<any> {
-    let randomFilm = Math.floor(Math.random() * 440000) + 1;
-    const randomFilmUrl = 'https://api.themoviedb.org/3/movie/'+ randomFilm;
-    return this.https.get(randomFilmUrl + this.key);
+  public getRandomFilm(voteAvgGte: string|number = null, yearGte: string|number = null, yearLte: string|number = null, orgCountry: string = null): Observable<any> {
+    const includeAdult = false;
+    const yearGteStr = yearGte === null ? '' : '&primary_release_date.gte=' + yearGte + '-01-01';
+    const yearLteStr = yearLte === null ? '' : '&primary_release_date.lte=' + yearLte + '-01-01';
+    const voteAvgGteStr = voteAvgGte === null ? '' : '&vote_average.gte=' + voteAvgGte + '&vote_count.gte=100';
+    const orgCountryStr = orgCountry === null ? '' : '&with_original_language=' + orgCountry;
+
+    const url = 'https://api.themoviedb.org/3/discover/movie'
+                + this.key
+                + '&include_adult=' + includeAdult
+                + yearGteStr
+                + yearLteStr
+                + voteAvgGteStr
+                + orgCountryStr
+    const obs = new Observable(observer => {
+      this.https.get(url).subscribe((response: any) => {
+        const page = Math.floor(Math.random() * response.total_pages) + 1;
+        const randomPage = '&page=' + page;
+        const randomFilm = Math.floor(Math.random() * response.results.length - 1);
+        const urlWithPage = url + randomPage;
+        this.https.get(urlWithPage).subscribe((finalResponse: any) => {
+          const filmId = finalResponse.results[randomFilm].id;
+          this.https.get('https://api.themoviedb.org/3/movie/'+ filmId + this.key).subscribe((film: any) => {
+            observer.next(film);
+          })
+        });
+      });
+    })
+    return obs;
   }
-  public searchTest(): Observable<any> {{
-    const query = '&query=a';
-    const url = 'https://api.themoviedb.org/3/search/movie' + this.key + query + '&language=en-US&page=1&include_adult=false&year=2018'
-    return this.https.get(url);
-  }}
 }
